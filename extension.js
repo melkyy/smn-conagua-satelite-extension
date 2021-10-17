2b263fcdeaf7bb8ee96413a7f38b5eccd6695de9
Aaron Rodriguez <melky.up@gmail.com>
2021-10-16 17:44:04 -0700

    arregle el desorden pero todavia falta pulirse

--- extension.js
+++ extension.js
@@ -1,23 +1,53 @@
 debugger;
-document.addEventListener("DOMContentLoaded", function(event) {
-    getConaguaTimeRange();
+document.addEventListener("DOMContentLoaded", async function(event) {
+    setTimeout(function(){
+        cargarTodo();
+        document.getElementById('loader').style.display = 'none';
+        document.getElementById('imagenes').style.display = 'block';
+    }, 1500);
+
   });
-  function getConaguaTimeRange(){
+function cargarTodo(){
+        const data = getImageUrl();
+        loadImages(data);
+        runAnimation(data);
+}
+  async function loadImages(arrImages){
+    const img = [];
+    for (let i = 0; i < arrImages.length; i++) {
+        const element = arrImages[i];
+        const img = document.createElement("img");
+        img.id = i;
+        img.src = 'https://smn.conagua.gob.mx/tools/RESOURCES/GOES/GOES%20Este/M%C3%A9xico/Tope%20de%20Nubes/MEDIA/'+element+'.jpg';
+        img.style.display = 'none';
+        img.style.height = '500px'; 
+        if(img){
+            document.getElementById('imagenes').appendChild(img);
+        }
+    }
+  }
+    function getImageUrl(){
     const fecha = moment().add(5, 'hours');
     const arrayFechas = [];
-    for (let i = 1; i < 9; i++) {
+    for (let i = 1; i < 10; i++) {
         const newFecha = moment(fecha).add((i * 10), 'm');
         arrayFechas.push((newFecha.format('YYYYMMDDHHmm')).replace(/.$/,"0"));
     }
-
-    var newsArray = arrayFechas;   // your code puts strings into this array
-var curNewsIndex = -1;
-
-var intervalID = setInterval(function() {
-    ++curNewsIndex;
-    if (curNewsIndex >= newsArray.length) {
-        curNewsIndex = 0;
+    return arrayFechas;
     }
-    document.getElementById('images').src = 'https://smn.conagua.gob.mx/tools/RESOURCES/GOES/GOES%20Este/M%C3%A9xico/Tope%20de%20Nubes/MEDIA/'+newsArray[curNewsIndex]+'.jpg';   // set new news item into the ticker
+
+function runAnimation(arrayFechas){
+    var index = 0;
+    console.log(arrayFechas);
+    var intervalID = setInterval(function() {
+    ++index;
+    if (index >= arrayFechas.length) {
+        document.getElementById(arrayFechas.length-1).style.display = 'none';
+        index = 0;
+    }else{
+        document.getElementById(index - 1).style.display = 'none';
+    }
+    document.getElementById(index).style.display = ' block'
+    console.log(index);
 }, 500);
 }
