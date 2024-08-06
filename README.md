0fd5850b1f98c9f9500814eb9a92e10e91eefa67
Aaron Rodriguez <melky.up@gmail.com>
2024-08-06 09:14:22 -0700

    Add manifest v3 and delete moment.js

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # smn-conagua-satelite-extension
-Extension de chrome para ver el clima por medio de imagenes de satelite gracias a la pagina de conagua
+
+Google extension for viewing weather forecasts using satellite images
 
 ![alt text](https://media4.giphy.com/media/JD66Ru5cD0gX0kQ5Uj/giphy.gif?cid=790b76119c835afabbbec78b09dccbd6bded84cc610e4e85&rid=giphy.gif&ct=g "Logo Title Text 1")
-
--- extension.js
+++ extension.js
@@ -1,54 +1,72 @@
 debugger;
-document.addEventListener("DOMContentLoaded", async function(event) {
-    setTimeout(function(){
-        cargarTodo();
-        document.getElementById('loader').style.display = 'none';
-        document.getElementById('imagenes').style.display = 'block';
-    }, 1500);
+const CONAGUA_URL =
+  "https://smn.conagua.gob.mx/tools/RESOURCES/GOES/GOES%20Este/M%C3%A9xico/Tope%20de%20Nubes/MEDIA/";
 
-  });
-function cargarTodo(){
-        const data = getImageUrl();
-        loadImages(data);
-        runAnimation(data);
+document.addEventListener("DOMContentLoaded", async function (event) {
+  await load();
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("images").style.display = "block";
+});
+
+async function load() {
+  const data = getImageUrl();
+  const images = await loadImages(data);
+  setTimeout(() => {
+    images.forEach((el) => {
+      document.getElementById("images").appendChild(el);
+    });
+  }, 1000);
+  runAnimation(data);
 }
-  async function loadImages(arrImages){
-    const img = [];
-    for (let i = 0; i < arrImages.length; i++) {
-        const element = arrImages[i];
-        const img = document.createElement("img");
-        img.id = i;
-        img.src = 'https://smn.conagua.gob.mx/tools/RESOURCES/GOES/GOES%20Este/M%C3%A9xico/Tope%20de%20Nubes/MEDIA/'+element+'.jpg';
-        img.style.display = 'none';
-        img.style.height = '500px';
-        img.loading="eager";
-        if(img){
-            document.getElementById('imagenes').appendChild(img);
-        }
+async function loadImages(arrImages) {
+  debugger;
+  const images = [];
+  for (let i = 0; i < arrImages.length; i++) {
+    const element = arrImages[i];
+    const img = document.createElement("img");
+    img.id = i;
+    img.src = CONAGUA_URL + element + ".jpg";
+    img.style.display = "none";
+    img.style.height = "500px";
+    img.loading = "eager";
+    if (img) {
+      images.push(img);
     }
   }
-    function getImageUrl(){
-    const fecha = moment().add(5, 'hours');
-    const arrayFechas = [];
-    for (let i = 1; i < 10; i++) {
-        const newFecha = moment(fecha).add((i * 10), 'm');
-        arrayFechas.push((newFecha.format('YYYYMMDDHHmm')).replace(/.$/,"0"));
-    }
-    return arrayFechas;
-    }
+  return images;
+}
+function pad2(n) {
+  return n < 10 ? "0" + n : n;
+}
 
-function runAnimation(arrayFechas){
-    var index = 0;
-    console.log(arrayFechas);
-    var intervalID = setInterval(function() {
+function getImageUrl() {
+  const date = new Date(new Date().setHours(new Date().getHours() + 5));
+  const datesArr = [];
+  for (let i = 1; i < 10; i++) {
+    const minutes = i * 10;
+    const newDate = new Date(date.getTime() + minutes * 60000);
+    const formatedDate =
+      newDate.getFullYear().toString() +
+      pad2(newDate.getMonth() + 1) +
+      pad2(newDate.getDate()) +
+      pad2(newDate.getHours()) +
+      pad2(Math.floor(newDate.getMinutes() / 10) * 10);
+
+    datesArr.push(formatedDate);
+  }
+  return datesArr;
+}
+
+function runAnimation(arrayFechas) {
+  var index = 0;
+  setInterval(function () {
     ++index;
     if (index >= arrayFechas.length) {
-        document.getElementById(arrayFechas.length-1).style.display = 'none';
-        index = 0;
-    }else{
-        document.getElementById(index - 1).style.display = 'none';
+      document.getElementById(arrayFechas.length - 1).style.display = "none";
+      index = 0;
+    } else {
+      document.getElementById(index - 1).style.display = "none";
     }
-    document.getElementById(index).style.display = ' block'
-    console.log(index);
-}, 500);
+    document.getElementById(index).style.display = " block";
+  }, 500);
 }
